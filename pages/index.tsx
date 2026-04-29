5803024307e5ba7c187361ed809d37d1e60a5824
FLOYD <j.shaymanov786@gmail.com>
2026-04-30 00:51:39 +0900

    feat: organize and test pages routing system

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,26 +35,7 @@ export default function Home() {
             priority
           />
           <div className={styles.intro}>
-            <h1>To get started, edit the index.tsx file.</h1>
-            <p>
-              Looking for a starting point or more instructions? Head over to{" "}
-              <a
-                href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Templates
-              </a>{" "}
-              or the{" "}
-              <a
-                href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learning
-              </a>{" "}
-              center.
-            </p>
+            <h1>PAGES ROUTING</h1>
           </div>
           <div className={styles.ctas}>
             <a
